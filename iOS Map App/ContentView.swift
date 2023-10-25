bb3f5a3fcb28639ac2d621174d495e44536eb9ae
Nathan Ulrich <nateulrich@gmail.com>
2023-10-25 08:22:57 -0400

    Initial Commit

---
+++ iOS Map App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Map App
+//
+//  Created by Nathan Ulrich on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Map App/iOS_Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Map_AppApp.swift
+//  iOS Map App
+//
+//  Created by Nathan Ulrich on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
